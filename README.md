dabee8f151dd46bbb9fe7e4c3d79129b585f03ef
Wolfgang Fahl <wf@bitplan.com>
2018-08-25 17:07:14 +0200

    fixes #1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [javafx controls and utilities - with binding to platform independent gui declaration](http://www.bitplan.com/Com.bitplan.javafx) 
 
 [![Travis (.org)](https://img.shields.io/travis/BITPlan/com.bitplan.javafx.svg)](https://travis-ci.org/BITPlan/com.bitplan.javafx)
-[![Maven Central](https://img.shields.io/maven-central/v/com.bitplan.gui/com.bitplan.javafx.svg)](https://search.maven.org/artifact/com.bitplan.gui/com.bitplan.javafx/0.0.18/jar)
+[![Maven Central](https://img.shields.io/maven-central/v/com.bitplan.gui/com.bitplan.javafx.svg)](https://search.maven.org/artifact/com.bitplan.gui/com.bitplan.javafx/0.0.19/jar)
 [![GitHub issues](https://img.shields.io/github/issues/BITPlan/com.bitplan.javafx.svg)](https://github.com/BITPlan/com.bitplan.javafx/issues)
 [![GitHub issues](https://img.shields.io/github/issues-closed/BITPlan/com.bitplan.javafx.svg)](https://github.com/BITPlan/com.bitplan.javafx/issues/?q=is%3Aissue+is%3Aclosed)
 [![GitHub](https://img.shields.io/github/license/BITPlan/com.bitplan.javafx.svg)](https://www.apache.org/licenses/LICENSE-2.0)
@@ -20,11 +20,11 @@ Maven dependency
 <dependency>
   <groupId>com.bitplan.gui</groupId>
   <artifactId>com.bitplan.javafx</artifactId>
-  <version>0.0.18</version>
+  <version>0.0.19</version>
 </dependency>
 ```
 
-[Current release at repo1.maven.org](http://repo1.maven.org/maven2/com/bitplan/gui/com.bitplan.javafx/0.0.18/)
+[Current release at repo1.maven.org](http://repo1.maven.org/maven2/com/bitplan/gui/com.bitplan.javafx/0.0.19/)
 
 ### How to build
 ```
@@ -33,21 +33,24 @@ cd com.bitplan.javafx
 mvn install
 ```
 ### Version History
-* 2017-08-20 0.0.1  - initial release
-* 2017-08-21 0.0.2  - adds XYTabPane
-* 2017-08-22 0.0.3  - improves icon handling
-* 2017-08-22 0.0.4  - adds GenericApp
-* 2017-08-23 0.0.5  - add JFXML
-* 2017-08-24 0.0.6  - fixes selectRandomTab
-* 2018-08-05 0.0.7  - upgrades to com.bitplan.gui 0.0.4 and refactors
-* 2018-08-05 0.0.8  - fixes incomplete release to maven central 
-* 2018-08-06 0.0.9  - improves I18n handling and fixes GenericApp
-* 2018-08-06 0.0.10 - refactors to use getI18nId() 
-* 2018-08-07 0.0.11 - refactors to use getInstance - improves I18n 
-* 2018-08-07 0.0.12 - includes medusa gauges and some extensions of it 
-* 2018-08-07 0.0.13 - refactors to include JFXStopWatch
-* 2018-08-10 0.0.14 - fixes Double handling - trim text
-* 2018-08-10 0.0.15 - upgrades com.bitplan.gui to 0.0.10 to fix getInstance
-* 2018-08-12 0.0.16 - upgrades com.bitplan.gui to 0.0.11 to improve JsonAble
-* 2018-08-15 0.0.17 - refactors WaitableApp to allow showTime directly
-* 2018-08-12 0.0.18 - upgrades com.bitplan.gui to 0.0.12 to improve JsonAble
+| Date       | Version | changes
+| -----------| ------: | ----------
+| 2017-08-20 |  0.0.1  | initial release
+| 2017-08-21 |  0.0.2  | adds XYTabPane
+| 2017-08-22 |  0.0.3  | improves icon handling
+| 2017-08-22 |  0.0.4  | adds GenericApp
+| 2017-08-23 |  0.0.5  | add JFXML
+| 2017-08-24 |  0.0.6  | fixes selectRandomTab
+| 2018-08-05 |  0.0.7  | upgrades to com.bitplan.gui 0.0.4 and refactors
+| 2018-08-05 |  0.0.8  | fixes incomplete release to maven central 
+| 2018-08-06 |  0.0.9  | improves I18n handling and fixes GenericApp
+| 2018-08-06 | 0.0.10  | refactors to use getI18nId() 
+| 2018-08-07 | 0.0.11  | refactors to use getInstance - improves I18n 
+| 2018-08-07 | 0.0.12  | includes medusa gauges and some extensions of it 
+| 2018-08-07 | 0.0.13  | refactors to include JFXStopWatch
+| 2018-08-10 | 0.0.14  | fixes Double handling - trim text
+| 2018-08-10 | 0.0.15  | upgrades com.bitplan.gui to 0.0.10 to fix getInstance
+| 2018-08-12 | 0.0.16  | upgrades com.bitplan.gui to 0.0.11 to improve JsonAble
+| 2018-08-15 | 0.0.17  | refactors WaitableApp to allow showTime directly
+| 2018-08-22 | 0.0.18  | upgrades com.bitplan.gui to 0.0.12 to improve JsonAble
+| 2018-08-25 | 0.0.19  | saveAsPng for nodes - fixes #1 
--- src/main/java/com/bitplan/javafx/WaitableApp.java
+++ src/main/java/com/bitplan/javafx/WaitableApp.java
@@ -40,7 +40,10 @@ import com.bitplan.gui.Display;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Bounds;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelReader;
@@ -58,239 +61,250 @@ import sun.awt.image.IntegerComponentRaster;
  */
 @SuppressWarnings("restriction")
 public abstract class WaitableApp extends Application implements Display {
-  protected static Logger LOGGER = Logger.getLogger("com.bitplan.javafx");
-  public static boolean debug = false;
-  public static boolean testMode = false;
-  protected Stage stage;
-  static boolean toolkitStarted;
-  File screenShot = null;
+	protected static Logger LOGGER = Logger.getLogger("com.bitplan.javafx");
+	public static boolean debug = false;
+	public static boolean testMode = false;
+	protected Stage stage;
+	static boolean toolkitStarted;
+	File screenShot = null;
 
-  public static double getScreenWidth() {
-    return Screen.getPrimary().getVisualBounds().getWidth();
-  }
+	public static double getScreenWidth() {
+		return Screen.getPrimary().getVisualBounds().getWidth();
+	}
 
-  public static double getScreenHeight() {
-    return Screen.getPrimary().getVisualBounds().getHeight();
-  }
+	public static double getScreenHeight() {
+		return Screen.getPrimary().getVisualBounds().getHeight();
+	}
 
-  /**
-   * allow startup without launch
-   */
-  public static void toolkitInit() {
-    if (!toolkitStarted) {
-      toolkitStarted = true;
-      // do not exit on close of last window
-      // https://stackoverflow.com/a/10217157/1497139
-      Platform.setImplicitExit(false);
-      /// https://stackoverflow.com/a/38883432/1497139
-      // http://www.programcreek.com/java-api-examples/index.php?api=com.sun.javafx.application.PlatformImpl
-      com.sun.javafx.application.PlatformImpl.startup(() -> {
-      });
-    }
-  }
+	/**
+	 * allow startup without launch
+	 */
+	public static void toolkitInit() {
+		if (!toolkitStarted) {
+			toolkitStarted = true;
+			// do not exit on close of last window
+			// https://stackoverflow.com/a/10217157/1497139
+			Platform.setImplicitExit(false);
+			/// https://stackoverflow.com/a/38883432/1497139
+			// http://www.programcreek.com/java-api-examples/index.php?api=com.sun.javafx.application.PlatformImpl
+			com.sun.javafx.application.PlatformImpl.startup(() -> {
+			});
+		}
+	}
 
-  /**
-   * get SceneBounds
-   * 
-   * @param screenPercent
-   * @return the scene bounds
-   */
-  public Rectangle2D getSceneBounds(int screenPercent, int xDiv, int yDiv) {
-    double sceneWidth = getScreenWidth() * screenPercent / 100.0;
-    double sceneHeight = getScreenHeight() * screenPercent / 100.0;
-    double x = (getScreenWidth() - sceneWidth) / xDiv;
-    double y = (getScreenHeight() - sceneHeight) / yDiv;
-    Rectangle2D sceneBounds = new Rectangle2D(x, y, sceneWidth, sceneHeight);
-    return sceneBounds;
-  }
+	/**
+	 * get SceneBounds
+	 * 
+	 * @param screenPercent
+	 * @return the scene bounds
+	 */
+	public Rectangle2D getSceneBounds(int screenPercent, int xDiv, int yDiv) {
+		double sceneWidth = getScreenWidth() * screenPercent / 100.0;
+		double sceneHeight = getScreenHeight() * screenPercent / 100.0;
+		double x = (getScreenWidth() - sceneWidth) / xDiv;
+		double y = (getScreenHeight() - sceneHeight) / yDiv;
+		Rectangle2D sceneBounds = new Rectangle2D(x, y, sceneWidth, sceneHeight);
+		return sceneBounds;
+	}
 
-  @Override
-  public void start(Stage stage) {
-    this.stage = stage;
-  }
+	@Override
+	public void start(Stage stage) {
+		this.stage = stage;
+	}
 
-  public Stage getStage() {
-    return stage;
-  }
+	public Stage getStage() {
+		return stage;
+	}
 
-  public void setStage(Stage stage) {
-    this.stage = stage;
-  }
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
 
-  @Override
-  public void browse(String url) {
-    this.getHostServices().showDocument(url);
-  }
+	@Override
+	public void browse(String url) {
+		this.getHostServices().showDocument(url);
+	}
 
-  public static int waitTimeOutSecs=10;
-  
-  public void abortWait() {
-    LOGGER.log(Level.WARNING,"wait aborted after "+waitTimeOutSecs+" secs");
-    if (!testMode)
-      System.exit(1);
-  }
-  
-  /**
-   * wait for close
-   * 
-   * @throws InterruptedException
-   */
-  public void waitStatus(boolean open) {
-    int sleep = 1000 / 50; // human eye reaction time
-    int timeout=0;
-    try {
-      if (open)
-        while ((stage == null) || (!stage.isShowing())) {
-          Thread.sleep(sleep);
-          timeout+=sleep;
-          if (timeout>=waitTimeOutSecs*1000) {
-            abortWait();
-            break;
-          }
-        }
-      else
-        while (stage != null && stage.isShowing()) {
-          Thread.sleep(sleep);
-          timeout+=sleep;
-        }
-    } catch (InterruptedException e) {
-      ErrorHandler.handle(e);
-    }
-  }
+	public static int waitTimeOutSecs = 10;
 
-  public void waitOpen() {
-    waitStatus(true);
-  }
+	public void abortWait() {
+		LOGGER.log(Level.WARNING, "wait aborted after " + waitTimeOutSecs + " secs");
+		if (!testMode)
+			System.exit(1);
+	}
 
-  public void waitClose() {
-    waitStatus(false);
-  }
+	/**
+	 * wait for close
+	 * 
+	 * @throws InterruptedException
+	 */
+	public void waitStatus(boolean open) {
+		int sleep = 1000 / 50; // human eye reaction time
+		int timeout = 0;
+		try {
+			if (open)
+				while ((stage == null) || (!stage.isShowing())) {
+					Thread.sleep(sleep);
+					timeout += sleep;
+					if (timeout >= waitTimeOutSecs * 1000) {
+						abortWait();
+						break;
+					}
+				}
+			else
+				while (stage != null && stage.isShowing()) {
+					Thread.sleep(sleep);
+					timeout += sleep;
+				}
+		} catch (InterruptedException e) {
+			ErrorHandler.handle(e);
+		}
+	}
 
-  /**
-   * show me
-   */
-  public void show() {
-    // ignore multiple calls
-    if (stage != null) {
-      LOGGER.log(Level.WARNING,"show called with active stage "+stage);
-      return;
-    }
-    Platform.runLater(() -> {
-      try {
-        Stage lStage = new Stage();
-        this.start(lStage);
-      } catch (Exception e) {
-        ErrorHandler.handle(e);
-      }
-    });
-  }
+	public void waitOpen() {
+		waitStatus(true);
+	}
 
-  /**
-   * limit the show Time
-   * 
-   * @param showTimeSecs
-   */
-  public void limitShowTime(int showTimeSecs) {
-    new Timer().schedule(new TimerTask() {
+	public void waitClose() {
+		waitStatus(false);
+	}
 
-      @Override
-      public void run() {
-        close();
-      }
-    }, showTimeSecs * 1000);
-  }
+	/**
+	 * show me
+	 */
+	public void show() {
+		// ignore multiple calls
+		if (stage != null) {
+			LOGGER.log(Level.WARNING, "show called with active stage " + stage);
+			return;
+		}
+		Platform.runLater(() -> {
+			try {
+				Stage lStage = new Stage();
+				this.start(lStage);
+			} catch (Exception e) {
+				ErrorHandler.handle(e);
+			}
+		});
+	}
 
-  /**
-   * close this display
-   */
-  public void close() {
-    Platform.runLater(() -> {
-      if (stage != null) {
-        stage.close();
-      }
-    });
-    this.waitClose();
-    // allow reopening
-    stage = null;
-  }
+	/**
+	 * limit the show Time
+	 * 
+	 * @param showTimeSecs
+	 */
+	public void limitShowTime(int showTimeSecs) {
+		new Timer().schedule(new TimerTask() {
 
-  /**
-   * save me as a Png File
-   * 
-   * @param stage
-   *          - the stage to take a screen shot from
-   * @param file
-   *          - the file to save the image to
-   */
-  public static synchronized void saveAsPng(Stage stage, File file) {
-    WritableImage image = stage.getScene().snapshot(null);
-    try {
-      ImageIO.write(fromFXImage(image, null), "png", file);
-    } catch (Throwable th) {
-      ErrorHandler.handle(th);
-    }
-  }
+			@Override
+			public void run() {
+				close();
+			}
+		}, showTimeSecs * 1000);
+	}
 
-  /**
-   * Snapshots the specified JavaFX {@link Image} object and stores a copy of
-   * its pixels into a {@link BufferedImage} object, creating a new object if
-   * needed. The method will only convert a JavaFX {@code Image} that is
-   * readable as per the conditions on the {@link Image#getPixelReader()
-   * Image.getPixelReader()} method. If the {@code Image} is not readable, as
-   * determined by its {@code getPixelReader()} method, then this method will
-   * return null. If the {@code Image} is a writable, or other dynamic image,
-   * then the {@code BufferedImage} will only be set to the current state of the
-   * pixels in the image as determined by its {@link PixelReader}. Further
-   * changes to the pixels of the {@code Image} will not be reflected in the
-   * returned {@code BufferedImage}.
-   * <p>
-   * The optional {@code BufferedImage} parameter may be reused to store the
-   * copy of the pixels. A new {@code BufferedImage} will be created if the
-   * supplied object is null, is too small or of a type which the image pixels
-   * cannot be easily converted into.
-   * 
-   * @param img
-   *          the JavaFX {@code Image} to be converted
-   * @param bimg
-   *          an optional {@code BufferedImage} object that may be used to store
-   *          the returned pixel data
-   * @return a {@code BufferedImage} containing a snapshot of the JavaFX
-   *         {@code Image}, or null if the {@code Image} is not readable.
-   * @since JavaFX 2.2
-   */
-  public static BufferedImage fromFXImage(Image img, BufferedImage bimg) {
-    PixelReader pr = img.getPixelReader();
-    if (pr == null) {
-      return null;
-    }
-    int iw = (int) img.getWidth();
-    int ih = (int) img.getHeight();
-    if (bimg != null) {
-      int type = bimg.getType();
-      int bw = bimg.getWidth();
-      int bh = bimg.getHeight();
-      if (bw < iw || bh < ih || (type != BufferedImage.TYPE_INT_ARGB
-          && type != BufferedImage.TYPE_INT_ARGB_PRE)) {
-        bimg = null;
-      } else if (iw < bw || ih < bh) {
-        Graphics2D g2d = bimg.createGraphics();
-        g2d.setComposite(AlphaComposite.Clear);
-        g2d.fillRect(0, 0, bw, bh);
-        g2d.dispose();
-      }
-    }
-    if (bimg == null) {
-      bimg = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_ARGB_PRE);
-    }
-    IntegerComponentRaster icr = (IntegerComponentRaster) bimg.getRaster();
-    int offset = icr.getDataOffset(0);
-    int scan = icr.getScanlineStride();
-    int data[] = icr.getDataStorage();
-    WritablePixelFormat<IntBuffer> pf = (bimg.isAlphaPremultiplied()
-        ? PixelFormat.getIntArgbPreInstance()
-        : PixelFormat.getIntArgbInstance());
-    pr.getPixels(0, 0, iw, ih, pf, data, offset, scan);
-    return bimg;
-  }
+	/**
+	 * close this display
+	 */
+	public void close() {
+		Platform.runLater(() -> {
+			if (stage != null) {
+				stage.close();
+			}
+		});
+		this.waitClose();
+		// allow reopening
+		stage = null;
+	}
+
+	/**
+	 * save me as a Png File
+	 * 
+	 * @param stage
+	 *            - the stage to take a screen shot from
+	 * @param file
+	 *            - the file to save the image to
+	 */
+	public static synchronized void saveAsPng(Stage stage, File file) {
+		saveAsPng(stage.getScene().getRoot(), file);
+	}
+
+	/**
+	 * save a snapshot of the given node as a PNG file
+	 * @param node - the node to get a snapshot from
+	 * @param file - the file to save to
+	 */
+	public static synchronized void saveAsPng(Node node, File file) {
+		Bounds bounds = node.getBoundsInParent();
+		WritableImage writableImage = new WritableImage((int) bounds.getWidth(), (int) bounds.getHeight());
+		SnapshotParameters params = new SnapshotParameters();
+		node.snapshot(params, writableImage);
+		try {
+			ImageIO.write(fromFXImage(writableImage, null), "png", file);
+		} catch (Throwable th) {
+			ErrorHandler.handle(th);
+		}
+	}
+
+	/**
+	 * Snapshots the specified JavaFX {@link Image} object and stores a copy of its
+	 * pixels into a {@link BufferedImage} object, creating a new object if needed.
+	 * The method will only convert a JavaFX {@code Image} that is readable as per
+	 * the conditions on the {@link Image#getPixelReader() Image.getPixelReader()}
+	 * method. If the {@code Image} is not readable, as determined by its
+	 * {@code getPixelReader()} method, then this method will return null. If the
+	 * {@code Image} is a writable, or other dynamic image, then the
+	 * {@code BufferedImage} will only be set to the current state of the pixels in
+	 * the image as determined by its {@link PixelReader}. Further changes to the
+	 * pixels of the {@code Image} will not be reflected in the returned
+	 * {@code BufferedImage}.
+	 * <p>
+	 * The optional {@code BufferedImage} parameter may be reused to store the copy
+	 * of the pixels. A new {@code BufferedImage} will be created if the supplied
+	 * object is null, is too small or of a type which the image pixels cannot be
+	 * easily converted into.
+	 * 
+	 * @param img
+	 *            the JavaFX {@code Image} to be converted
+	 * @param bimg
+	 *            an optional {@code BufferedImage} object that may be used to store
+	 *            the returned pixel data
+	 * @return a {@code BufferedImage} containing a snapshot of the JavaFX
+	 *         {@code Image}, or null if the {@code Image} is not readable.
+	 * @since JavaFX 2.2
+	 */
+	public static BufferedImage fromFXImage(Image img, BufferedImage bimg) {
+		PixelReader pr = img.getPixelReader();
+		if (pr == null) {
+			return null;
+		}
+		int iw = (int) img.getWidth();
+		int ih = (int) img.getHeight();
+		if (bimg != null) {
+			int type = bimg.getType();
+			int bw = bimg.getWidth();
+			int bh = bimg.getHeight();
+			if (bw < iw || bh < ih
+					|| (type != BufferedImage.TYPE_INT_ARGB && type != BufferedImage.TYPE_INT_ARGB_PRE)) {
+				bimg = null;
+			} else if (iw < bw || ih < bh) {
+				Graphics2D g2d = bimg.createGraphics();
+				g2d.setComposite(AlphaComposite.Clear);
+				g2d.fillRect(0, 0, bw, bh);
+				g2d.dispose();
+			}
+		}
+		if (bimg == null) {
+			bimg = new BufferedImage(iw, ih, BufferedImage.TYPE_INT_ARGB_PRE);
+		}
+		IntegerComponentRaster icr = (IntegerComponentRaster) bimg.getRaster();
+		int offset = icr.getDataOffset(0);
+		int scan = icr.getScanlineStride();
+		int data[] = icr.getDataStorage();
+		WritablePixelFormat<IntBuffer> pf = (bimg.isAlphaPremultiplied() ? PixelFormat.getIntArgbPreInstance()
+				: PixelFormat.getIntArgbInstance());
+		pr.getPixels(0, 0, iw, ih, pf, data, offset, scan);
+		return bimg;
+	}
 
 }
