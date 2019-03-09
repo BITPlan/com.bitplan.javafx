6cb4345a2e66819e5d303fbc7ac88186361b2abd
Wolfgang Fahl <wf@bitplan.com>
2019-03-09 07:26:00 +0100

    modifies example

--- src/test/java/com/bitplan/javafx/stackoverflow/RubberBandSelectionDemo.java
+++ src/test/java/com/bitplan/javafx/stackoverflow/RubberBandSelectionDemo.java
@@ -25,11 +25,13 @@
  */
 package com.bitplan.javafx.stackoverflow;
 
-import javafx.application.Application;
+import com.bitplan.javafx.ImageViewPane;
+
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -38,155 +40,155 @@ import javafx.scene.shape.StrokeLineCap;
 import javafx.stage.Stage;
 
 /**
- * see https://stackoverflow.com/questions/30295071/how-to-create-stackpane-on-the-drawn-rectangle-area
+ * see
+ * https://stackoverflow.com/questions/30295071/how-to-create-stackpane-on-the-drawn-rectangle-area
+ * 
  * @author Roland
  *
  */
-public class RubberBandSelectionDemo extends Application {
+public class RubberBandSelectionDemo extends TestApplication {
 
-    CheckBox drawButtonCheckBox;
+  CheckBox drawButtonCheckBox;
 
-    public static void main(String[] args) {
-        launch(args);
+  @Override
+  public void start(Stage primaryStage) {
+    ImageView imageView = super.getImageView();
+    ImageViewPane imageViewPane=new ImageViewPane(imageView);
+    Pane pane = new Pane();
+    pane.getChildren().add(imageViewPane);
+    
+    drawButtonCheckBox = new CheckBox("Draw Button");
+    pane.getChildren().add(drawButtonCheckBox);
+    createSceneAndShowStage(primaryStage, pane);
+
+    new RubberBandSelection(pane);
+
+  }
+
+  public class RubberBandSelection {
+
+    final DragContext dragContext = new DragContext();
+    Rectangle rect;
+
+    Pane group;
+
+    public RubberBandSelection(Pane group) {
+
+      this.group = group;
+
+      rect = new Rectangle(0, 0, 0, 0);
+      rect.setStroke(Color.BLUE);
+      rect.setStrokeWidth(1);
+      rect.setStrokeLineCap(StrokeLineCap.ROUND);
+      rect.setFill(Color.LIGHTBLUE.deriveColor(0, 1.2, 1, 0.6));
+
+      group.addEventHandler(MouseEvent.MOUSE_PRESSED,
+          onMousePressedEventHandler);
+      group.addEventHandler(MouseEvent.MOUSE_DRAGGED,
+          onMouseDraggedEventHandler);
+      group.addEventHandler(MouseEvent.MOUSE_RELEASED,
+          onMouseReleasedEventHandler);
+
     }
 
+    EventHandler<MouseEvent> onMousePressedEventHandler = new EventHandler<MouseEvent>() {
 
-    Pane root;
+      @Override
+      public void handle(MouseEvent event) {
+        dragContext.mouseAnchorX = event.getSceneX();
+        dragContext.mouseAnchorY = event.getSceneY();
 
-    @Override
-    public void start(Stage primaryStage) {
+        rect.setX(dragContext.mouseAnchorX);
+        rect.setY(dragContext.mouseAnchorY);
+        rect.setWidth(0);
+        rect.setHeight(0);
+        add(rect);
 
-        root = new Pane();
-        root.setStyle("-fx-background-color:white");
-        root.setPrefSize(1024, 768);
+      }
+    };
 
-        drawButtonCheckBox = new CheckBox( "Draw Button");
-        root.getChildren().add( drawButtonCheckBox);
+    EventHandler<MouseEvent> onMouseReleasedEventHandler = new EventHandler<MouseEvent>() {
 
-        primaryStage.setScene(new Scene(root, root.getWidth(), root.getHeight()));
-        primaryStage.show();
+      @Override
+      public void handle(MouseEvent event) {
 
-        new RubberBandSelection(root);
+        // get coordinates
+        double x = rect.getX();
+        double y = rect.getY();
+        double w = rect.getWidth();
+        double h = rect.getHeight();
 
-    }
+        if (drawButtonCheckBox.isSelected()) {
 
+          // create button
+          Button node = new Button();
+          node.setDefaultButton(false);
+          node.setPrefSize(w, h);
+          node.setText("Button");
+          node.setLayoutX(x);
+          node.setLayoutY(y);
+          add(node);
 
-    public class RubberBandSelection {
-
-        final DragContext dragContext = new DragContext();
-        Rectangle rect;
-
-        Pane group;
-
-        public RubberBandSelection( Pane group) {
-
-            this.group = group;
-
-            rect = new Rectangle( 0,0,0,0);
-            rect.setStroke(Color.BLUE);
-            rect.setStrokeWidth(1);
-            rect.setStrokeLineCap(StrokeLineCap.ROUND);
-            rect.setFill(Color.LIGHTBLUE.deriveColor(0, 1.2, 1, 0.6));
-
-            group.addEventHandler(MouseEvent.MOUSE_PRESSED, onMousePressedEventHandler);
-            group.addEventHandler(MouseEvent.MOUSE_DRAGGED, onMouseDraggedEventHandler);
-            group.addEventHandler(MouseEvent.MOUSE_RELEASED, onMouseReleasedEventHandler);
-
+        } else {
+          // create rectangle
+          Rectangle node = new Rectangle(0, 0, w, h);
+          node.setStroke(Color.BLACK);
+          node.setFill(Color.BLACK.deriveColor(0, 0, 0, 0.3));
+          node.setLayoutX(x);
+          node.setLayoutY(y);
+          add(node);
         }
 
-        EventHandler<MouseEvent> onMousePressedEventHandler = new EventHandler<MouseEvent>() {
+        // remove rubberband
+        rect.setX(0);
+        rect.setY(0);
+        rect.setWidth(0);
+        rect.setHeight(0);
 
-            @Override
-            public void handle(MouseEvent event) {
-                dragContext.mouseAnchorX = event.getSceneX();
-                dragContext.mouseAnchorY = event.getSceneY();
+        group.getChildren().remove(rect);
 
-                rect.setX(dragContext.mouseAnchorX);
-                rect.setY(dragContext.mouseAnchorY);
-                rect.setWidth(0);
-                rect.setHeight(0);
+      }
+    };
 
-                group.getChildren().add( rect);
+    EventHandler<MouseEvent> onMouseDraggedEventHandler = new EventHandler<MouseEvent>() {
 
-            }
-        };
+      @Override
+      public void handle(MouseEvent event) {
 
-        EventHandler<MouseEvent> onMouseReleasedEventHandler = new EventHandler<MouseEvent>() {
+        double offsetX = event.getSceneX() - dragContext.mouseAnchorX;
+        double offsetY = event.getSceneY() - dragContext.mouseAnchorY;
 
-            @Override
-            public void handle(MouseEvent event) {
-
-                // get coordinates
-                double x = rect.getX();
-                double y = rect.getY();
-                double w = rect.getWidth();
-                double h = rect.getHeight();
-
-                if( drawButtonCheckBox.isSelected()) {
-
-                    // create button
-                    Button node = new Button();
-                    node.setDefaultButton(false);
-                    node.setPrefSize(w, h);
-                    node.setText("Button");
-                    node.setLayoutX(x);
-                    node.setLayoutY(y);
-                    root.getChildren().add( node);
-
-
-                } else {
-                    // create rectangle
-                    Rectangle node = new Rectangle( 0, 0, w, h);
-                    node.setStroke( Color.BLACK);
-                    node.setFill( Color.BLACK.deriveColor(0, 0, 0, 0.3));
-                    node.setLayoutX( x);
-                    node.setLayoutY( y);
-                    root.getChildren().add( node);
-                }
-
-
-                // remove rubberband
-                rect.setX(0);
-                rect.setY(0);
-                rect.setWidth(0);
-                rect.setHeight(0);
-
-                group.getChildren().remove( rect);
-
-
-            }
-        };
-
-        EventHandler<MouseEvent> onMouseDraggedEventHandler = new EventHandler<MouseEvent>() {
-
-            @Override
-            public void handle(MouseEvent event) {
-
-                double offsetX = event.getSceneX() - dragContext.mouseAnchorX;
-                double offsetY = event.getSceneY() - dragContext.mouseAnchorY;
-
-                if( offsetX > 0)
-                    rect.setWidth( offsetX);
-                else {
-                    rect.setX(event.getSceneX());
-                    rect.setWidth(dragContext.mouseAnchorX - rect.getX());
-                }
-
-                if( offsetY > 0) {
-                    rect.setHeight( offsetY);
-                } else {
-                    rect.setY(event.getSceneY());
-                    rect.setHeight(dragContext.mouseAnchorY - rect.getY());
-                }
-            }
-        };
-
-        private final class DragContext {
-
-            public double mouseAnchorX;
-            public double mouseAnchorY;
-
-
+        if (offsetX > 0)
+          rect.setWidth(offsetX);
+        else {
+          rect.setX(event.getSceneX());
+          rect.setWidth(dragContext.mouseAnchorX - rect.getX());
         }
+
+        if (offsetY > 0) {
+          rect.setHeight(offsetY);
+        } else {
+          rect.setY(event.getSceneY());
+          rect.setHeight(dragContext.mouseAnchorY - rect.getY());
+        }
+      }
+    };
+
+    private final class DragContext {
+
+      public double mouseAnchorX;
+      public double mouseAnchorY;
+
     }
+
+    protected void add(Node node) {
+      group.getChildren().add(node);
+
+    }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
 }
