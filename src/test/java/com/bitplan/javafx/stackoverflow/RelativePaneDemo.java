61795254091884f092148c2ed5eeecc739789288
Wolfgang Fahl <wf@bitplan.com>
2019-04-01 11:22:15 +0200

    tries RelativePane

---
+++ src/test/java/com/bitplan/javafx/stackoverflow/RelativePaneDemo.java
@@ -0,0 +1,53 @@
+/**
+ *
+ * This file is part of the https://github.com/BITPlan/com.bitplan.javafx open source project
+ *
+ * The copyright and license below holds true
+ * for all files except
+ *    - the ones in the stackoverflow package
+ *    - SwingFXUtils.java from Oracle which is provided e.g. for the raspberry env
+ *
+ * Copyright 2017-2018 BITPlan GmbH https://github.com/BITPlan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *  http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bitplan.javafx.stackoverflow;
+
+import com.bitplan.javafx.RelativePane;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RelativePaneDemo extends TestApplication {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    
+    RelativePane percentPane=new RelativePane();
+    percentPane.addControl(new Button("tl"), 0, 0, 0.2, 0.2);
+    percentPane.addControl(new Button("middle"),0.25,0.25,0.5,0.5);
+    percentPane.addControl(new Button("br"), 0.8, 0.8, 0.2, 0.2);
+    Scene scene = new Scene(percentPane, 300, 250);
+    primaryStage.setTitle("percentPane");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+    
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
